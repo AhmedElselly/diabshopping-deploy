0dd7ee2eecdd60f5a276901a45e74d67394466f4
ahmedelselly <ahmedelselly@localhost.localdomain>
2022-02-08 12:13:03 +0200

    added case and default

--- pages/api/products/home.js
+++ pages/api/products/home.js
@@ -5,14 +5,27 @@ import Product from '../../../models/Product';
 const handler = async (req, res) => {
 	const {method} = req;
 	await dbConnect();
-	if(method === 'GET'){
-		try{
-			const products = await Product.find().limit(6).sort('-createdAt');
-			return res.status(200).json(products);
-		} catch(err){
-			res.status(400).json(err);
-		}
+	switch(method) {
+		case 'GET':
+			try{
+				const products = await Product.find().limit(6).sort('-createdAt');
+				return res.status(200).json(products);
+			} catch(err){
+				res.status(400).json(err);
+			}
+			break;
+		default:
+			res.status(400).json({success: false});
+			break;
 	}
+	// if(method === 'GET'){
+	// 	try{
+	// 		const products = await Product.find().limit(6).sort('-createdAt');
+	// 		return res.status(200).json(products);
+	// 	} catch(err){
+	// 		res.status(400).json(err);
+	// 	}
+	// }
 }
 
 export default handler;
